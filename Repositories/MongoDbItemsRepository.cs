5b35c976491216e742460e2e0d9e96187d7aedbe
Igor <ermygut@gmail.com>
2021-07-18 15:03:46 +0300

    MongoDB setup

---
+++ Repositories/MongoDbItemsRepository.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using Catalog.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Repositories
+{
+    public class MongoDbItemsRepository : IItemsRepository
+    {
+        private const string databaseName = "catalog";
+        private const string collectionName = "items";
+        private readonly IMongoCollection<Item> itemsCollection;
+        public MongoDbItemsRepository(IMongoClient mongoClient)
+        {
+
+        }
+        public void CreateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
